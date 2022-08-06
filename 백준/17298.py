ad71ece3daed3d9f42f14df7a71e3785eef7a93c
unknown <joyw93@naver.com>
2022-08-06 23:37:07 +0900

    solve

---
+++ 백준/17298.py
@@ -0,0 +1,17 @@
+# N = int(input())
+# nums = list(map(int, input().split()))
+
+N = 4
+nums = [3,5,2,7]
+
+result = [-1] * N
+stack = [0]
+
+for i in range(1, N):
+    while stack and nums[stack[-1]] < nums[i]:
+        end = stack.pop()
+        result[end] = nums[i]
+    stack.append(i)
+    
+print(*result)
+        
